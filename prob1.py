b36257ac8bfff5fd1210c9370a1878fa28571a4f
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-04-19 19:51:41 +0530

    Problem 1

---
+++ prob1.py
@@ -0,0 +1,8 @@
+# Take a number from user and print whether it is EVEN or ODD.
+
+n = int(input("Enter the number : "))
+if (n%2 == 0):
+    print("The number is Even")
+else:
+    print("The number is Odd")
+
