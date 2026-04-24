0384ab220af3424baf66acbd6948c76689b5e1be
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-04-24 15:39:07 +0530

    Probelm 13

---
+++ prob13.py
@@ -0,0 +1,10 @@
+# 1. Print each character of a string
+
+# n = input("Enter the string: ")
+
+# for i in range(0, len(n)):
+#     print(n[i])
+
+n = input ("Enter the string: ")
+for ch in n:
+    print(ch)
