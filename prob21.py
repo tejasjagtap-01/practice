ff8b664270a10a459ccf0ce50fb0e2258be250a6
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-04-29 19:20:40 +0530

    Added Comment

--- prob21.py
+++ prob21.py
@@ -1,16 +1,19 @@
 # First non-repeating character
+#assisted by ChatGpt
 
-s = input("Enter the String: ")
+s = input("Enter the String: ") #accepts the user values
 
-freq = {}
+freq = {} #create empty dictionary to store frequency 
 
-for ch in s:
-    if ch in freq:
-        freq[ch] += 1
+#counts frequency of each character
+for ch in s: #loop through each character in string
+    if ch in freq: #if character already existed in dictionary 
+        freq[ch] += 1 #increase its count
     else:
-        freq[ch] = 1
+        freq[ch] = 1 #if first step set count = 1
 
-for ch in s:
-    if freq[ch] == 1:
-        print("First non-repeating: ",ch)
-        break
+#find first non repeating characters
+for ch in s: #loop again in original order
+    if freq[ch] == 1: #check if frequency is 1
+        print("First non-repeating: ",ch) 
+        break # Stop after finding the first one 
