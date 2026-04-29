4b07ba9ef9b9921aa4ad59e0406bab02c1cfb4b1
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-04-29 19:11:52 +0530

    Problem 22

---
+++ prob22.py
@@ -0,0 +1,13 @@
+# Replace vowels with *
+
+s = input("Enter the String: ") #accept user input 
+
+result = "" #stores the the empty string to string to store result
+
+for ch in s: #loop through each character of the string
+    if ch in "aeiouAEIOU": #check for vowels
+        result += "*" #replace vowel with *
+    else:
+        result += ch #keep character as it is
+
+print(result) #print final modified output
