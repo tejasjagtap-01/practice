117951b7a7ea2fd842af3e7dd66269795424339f
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-05-02 15:29:53 +0530

    Problem 27

---
+++ prob27.py
@@ -0,0 +1,8 @@
+# Q3. Write a program to check if a number is even or odd. 
+
+s = int(input("Enter the number: "))
+
+if (s%2 == 0):
+    print("The number is Even")
+else:
+    print("The number is Odd")
