f4d2af896f2d9a438b1872b5a01bf9e73d3050ef
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-05-03 11:05:09 +0530

    Problem 30

---
+++ prob30.py
@@ -0,0 +1,7 @@
+#  Convert temperature from Celsius to Fahrenheit
+
+t = float(input("Enter the temperature in Celsius: "))
+
+f = (t * 1.8) + 32
+
+print(f"The temperature {t} after converting to fahrenheit is {f:.2f}")
