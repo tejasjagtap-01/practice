043fcab57af87e9746c94623706132bf262cf238
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-05-08 13:09:14 +0530

    Changes

--- prob14.py
+++ prob14.py
@@ -10,3 +10,15 @@ for ch in n:
     count += 1
 
 print(f"Length of String: {count}")
+
+
+s = input("Enter the string: ")
+
+count = 0
+
+
+# for i in range(0, 1000):   # assume max length
+#     if i < len(s):         # using len just for safety here
+#         count += 1
+
+# print(count)
