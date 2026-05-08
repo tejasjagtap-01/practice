286719c65a2e3e15fd497e7012350c360901c215
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-05-09 00:21:42 +0530

    Changes

--- prob32.py
+++ prob32.py
@@ -1,10 +1,12 @@
 #  Create a program to count the number of vowels in a string
 
-s = input("Enter the String: ")
+s = input("Enter the String: ")  # Take input string from user
 
-vowel = 0
+vowel = 0 # Variable to store count of vowels
 
+# Loop through each character in the string
 for ch in s:
-    if ch in "aeiouAEIOU":
-        vowel += 1
+    if ch in "aeiouAEIOU":  # Check if character is a vowel (both lowercase and uppercase)
+        vowel += 1 # Increase vowel count by 1
+        
 print("The Total Count",vowel)
