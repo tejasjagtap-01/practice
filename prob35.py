bb71e87925b326351f6798f074d9f19b6f53e8f3
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-05-09 23:48:52 +0530

    file 35

---
+++ prob35.py
@@ -0,0 +1,23 @@
+# x=1
+
+# if(x!=1):
+#     print('Hello')
+
+# else:
+#     print('Hi')
+# print('Mike')
+
+# A = ['1','2','3']
+
+# for a in A:
+#     print(2*a)
+
+# def Delta(x):
+    
+#     if x==0:
+#         y=1
+#     else:
+#         y=0
+#     return(y)
+
+a = {'a':1,'b':2}
