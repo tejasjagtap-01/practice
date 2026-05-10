c247abe9a1d8cb5ce9efa81abfc4d06d3ba1cf96
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-05-11 00:57:45 +0530

    Completed

---
+++ prob37.py
@@ -0,0 +1,17 @@
+s = input("Enter the String: ")
+
+left = 0
+right = len(s)-1
+is_Palindrome = True
+
+while left < right:
+    if s[left] != s[right]:
+        is_Palindrome = False
+        break
+    left += 1
+    right -= 1
+        
+if is_Palindrome:    
+    print("It is Palindrome")
+else:
+    print("It is not Palindrome")
